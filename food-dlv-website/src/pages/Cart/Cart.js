48e78f9edac1523b83f7beb3faf02103369016de
伯翰 侯 <john860703@gmail.com>
2023-03-20 23:17:49 +0800

    Ver 3.0.0 All functions are complete

--- food-dlv-website/src/pages/Cart/Cart.js
+++ food-dlv-website/src/pages/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import CartService from "../../services/Cart/cart.service";
 import ShoppingCart from "../../assets/images/shopping_cart.png";
